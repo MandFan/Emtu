5fbb6e7a6a9dcf82ec01bab185a019ac590556eb
MandFan <diyor160200@gmail.com>
2023-02-27 13:08:05 +0300

    1

---
+++ Windows/RegisWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using FitnessClubErmakov.Windows;
+using FitnessClubErmakov.DataBase;
+using FitnessClubErmakov.ClassHelper;
+
+namespace FitnessClubErmakov.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisWindow.xaml
+    /// </summary>
+    public partial class RegisWindow : Window
+    {
+        public RegisWindow()
+        {
+            InitializeComponent();
+            CMBGender.ItemsSource = ClassHelper.EFClass.context.GenderCode.ToList();
+            CMBGender.DisplayMemberPath = "Name";
+            CMBGender.SelectedIndex = 0;
+        }
+
+
+        // Кнопка "Уже есть аккаунт"
+        private void BtnAlreadyAccount_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow registrationClientWindow = new AuthWindow();
+            registrationClientWindow.ShowDialog();
+        }
+        // Кнопка "Регистрация"
+        private void BtnRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbSecondName.Text))
+            {
+                MessageBox.Show("Поле фамилия не может быть пустым.");
+                return;
+            }
+            if (string.IsNullOrEmpty(tbPassword.Text))
+            {
+                MessageBox.Show("Нужен пароль.");
+                return;
+            }
+
+            // Проверка совпедения паролей
+            if (tbPassword == tbRepPassword)
+            {
+                // Добавление клиента
+                Client client = new Client();
+
+                client.Name = tbFirstName.Text;
+                client.SecondName = tbSecondName.Text;
+                client.Patronimic = tbPatronimic.Text;
+                client.Phone = tbPhone.Text;
+                client.Email = tbEmail.Text;
+                client.GenderCode = (CMBGender.SelectedItem as GenderCode).GenderCode1;
+                client.Password = tbPassword.Text;
+
+                ClassHelper.EFClass.context.Client.Add(client);
+
+                ClassHelper.EFClass.context.SaveChanges();
+
+                MessageBox.Show("Регистрация успешна");
+            }
+            else 
+            {
+                MessageBox.Show("Пароли должны совпадать!");
+                return;
+            }
+
+        }
+    }
+}
---
+++ Windows/ServiceListWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using FitnessClubErmakov.ClassHelper;
+using FitnessClubErmakov.DataBase;
+using FitnessClubErmakov.Windows;
+
+namespace FitnessClubErmakov.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ServiceListWindow.xaml
+    /// </summary>
+    public partial class ServiceListWindow : Window
+    {
+        public ServiceListWindow()
+        {
+            InitializeComponent();
+            GetServiceList();
+        }
+
+        private void GetServiceList()
+        {
+            List<Service> serviceList = new List<Service>();
+
+             serviceList = EFClass.context.Service.ToList();
+
+            lvService.ItemsSource = serviceList;
+        }
+
+        // Изменение услуги
+        private void BtnEditProduct_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null)
+            {
+                return;
+            }
+
+            var service = button.DataContext as Service;
+
+
+            AddEditServiceWindow addEditServiceWindow = new AddEditServiceWindow(service);
+            addEditServiceWindow.ShowDialog();
+
+            GetServiceList();
+        }
+
+        // Добавление услуги
+        private void BtnAddService_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditServiceWindow addEditServiceWindow = new AddEditServiceWindow();
+            addEditServiceWindow.ShowDialog();
+
+            GetServiceList();
+        }
+    }
+}
